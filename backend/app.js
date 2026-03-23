f19839673585858cba28c9c0b5caddcb0dfa7a64
ivan_maleshko <183198079+imaleshko@users.noreply.github.com>
2026-03-23 15:52:36 +0200

    Fix 2

--- backend/app.js
+++ backend/app.js
@@ -99,7 +99,7 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
